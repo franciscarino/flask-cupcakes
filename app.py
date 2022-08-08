60bcba06350f59c876fbe405883ab73b98c861b5
Ryan Rustyn <ryanrustyn@gmail.com>
2022-08-08 12:38:16 -0700

    add get and post routes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """Flask app for Cupcakes"""
-from flask import Flask, url_for, render_template, redirect, flash, jsonify
+from flask import Flask, request, url_for, render_template, redirect, flash, jsonify
 
 from models import db, connect_db, Cupcake
 
@@ -12,6 +12,47 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///cupcakes"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-
 connect_db(app)
 db.create_all()
+
+############################################################
+# Routes
+
+
+@app.get("/api/cupcakes")
+def list_all_cupcakes():
+    """Return JSON {'cupcakes': [{id,flavor,size,rating,image},...]}"""
+
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+
+@app.get("/api/cupcakes/<int:cupcake_id>")
+def show_capcake_info(cupcake_id):
+    """Return JSON {'cupcake': {id,flavor,size,rating,image}}"""
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = cupcake.serialize()
+
+    return jsonify(cupcake=serialized)
+
+
+@app.post("/api/cupcakes")
+def create_cupcake():
+    """Return JSON {'cupcake': {id,flavor,size,rating,image}}"""
+
+    cupcake = Cupcake(
+        flavor=request.json["flavor"],
+        size=request.json["size"],
+        rating=request.json["rating"],
+        image=request.json["image"]
+    )
+
+    db.session.add(cupcake)
+    db.session.commit()
+
+    serialized= cupcake.serialize()
+
+    return (jsonify(cupcake= serialized), 201)
