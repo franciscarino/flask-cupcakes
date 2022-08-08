ed418f7fd1b0756c1a8b8ff99013118ccd33ab2b
Ryan Rustyn <ryanrustyn@gmail.com>
2022-08-08 12:08:49 -0700

    base app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 """Flask app for Cupcakes"""
+from flask import Flask, url_for, render_template, redirect, flash, jsonify
+
+from models import db, connect_db, Cupcake
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "hunter2"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///cupcakes"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+
+connect_db(app)
+db.create_all()
